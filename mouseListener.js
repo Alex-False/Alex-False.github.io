e027ee281c5bbc4f4605a2cca621c32d2f575f03
AlexFalse <96215838+Alex-False@users.noreply.github.com>
2023-01-27 05:17:24 -0700

    Foundational

--- mouseListener.js
+++
@@ -1,8 +0,0 @@
-const header = document.querySelector(".site-header");
-header.addEventListener("mouseover", function() {
-  header.classList.add("hover");
-});
-
-header.addEventListener("mouseout", function() {
-  header.classList.remove("hover");
-});
